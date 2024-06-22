28481acf6ca8c4ae0262985f04ef1f617b4231b8
Timur Turatbekov <timatur242005@gmail.com>
2024-06-22 14:34:15 +0300

    Added inertia and air speed boost

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -1,10 +1,18 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export var floor_acceleration = 50.0
+@export var air_acceleration = 20.0
+
+## How fast player will lose their velocity on the ground
+@export_range(0.0, 1.0, 0.01) var floor_friction = 0.1
+## How fast player will lose their velocity in the air
+@export_range(0.0, 1.0, 0.01) var air_friction = 0.02
+
+@export var jump_velocity = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -13,12 +21,26 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
+	
+	# Applying friction
+	if is_on_floor():
+		velocity.x *= 1 - floor_friction
+		velocity.z *= 1 - floor_friction
+	else: 
+		velocity.x *= 1 - air_friction
+		velocity.z *= 1 - air_friction
 
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backwards")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	velocity.x = direction.x * SPEED
-	velocity.z = direction.z * SPEED
+	var velocity_to_add = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if is_on_floor():
+		velocity_to_add *= floor_acceleration * delta
+	else:
+		velocity_to_add *= air_acceleration * delta
+	
+	velocity.x += velocity_to_add.x
+	velocity.z += velocity_to_add.z
 
 	move_and_slide()

f9f0c95bc29e00883db0748630c05f187edeba2d
Timur Turatbekov <timatur242005@gmail.com>
2024-06-22 14:54:25 +0300

    Fixed jump key

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -22,7 +22,7 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
 	
 	# Applying friction

eb1fed4bc8a2e6de8f074889af10c15a452d6352
Timur Turatbekov <timatur242005@gmail.com>
2024-06-24 19:13:19 +0300

    Added item textures

--- scripts/items/item.gd
+++ scripts/items/item.gd
@@ -1,8 +1,11 @@
 class_name Item
-extends Node
+extends Node3D
 
 @export var info: ItemInfo
+@export var height_amplitude = 0.5
+@export var frequency = 0.6
 
+@onready var start_y = position.y
 
 func _on_body_entered(body):
 	var movement = body as PlayerMovement
@@ -11,3 +14,7 @@ func _on_body_entered(body):
 		return
 
 	info.pickup(movement.player, self)
+
+
+func _process(delta):
+	position.y = start_y + sin(Time.get_ticks_msec() / 1000.0 * frequency) * height_amplitude

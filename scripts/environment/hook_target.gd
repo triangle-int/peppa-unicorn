035b8ba4ec75ee262e060254a88ca995180b807c
Timur Turatbekov <timatur242005@gmail.com>
2024-06-24 17:26:44 +0300

    Hook selection visuals

--- scripts/environment/hook_target.gd
+++ scripts/environment/hook_target.gd
@@ -1,2 +1,26 @@
 class_name HookTarget
 extends Node3D
+
+
+func _on_activation_area_body_entered(body):
+	var player = body as PlayerMovement
+	if player != null:
+		var hook = player.player.hook
+		hook.all_targets.push_back(self)
+
+
+func _on_activation_area_body_exited(body):
+	var player = body as PlayerMovement
+	if player != null:
+		var hook = player.player.hook
+		hook.all_targets.erase(self)
+
+
+func select():
+	$Sprite3D.scale = Vector3.ONE * 5.0
+	$Sprite3D.frame = 1
+
+
+func unselect():
+	$Sprite3D.scale = Vector3.ONE * 2.5
+	$Sprite3D.frame = 0
